88d1c60dac58c294c3b3e9cc13dd69e1d0ea57e7
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2024-03-24 16:52:59 +0100

    initial commit

---
+++ main.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/lua
+--[[
+  midi hardware sampler helper
+  
+  @dogsplusplus
+]]
+
+-- This function will be called when our menu item is selected.
+--local function show_message()
+--  renoise.app():show_message("Hello from your Renoise tool!")
+--end
+--
+---- Add the menu entry to the Tools menu.
+--renoise.tool():add_menu_entry {
+--  name = "Main Menu:Tools:Show Hello Message",
+--  invoke = show_message -- This function is called when the menu item is clicked.
+--}
+
+print("Test LUA")
+
+--[[============================================================================
+manifest.xml
+============================================================================]]--
